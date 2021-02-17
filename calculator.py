b53a8171fc2d6101a6ac3ff18f62a8303b850014
ishankatoch <ishankatoch@gmail.com>
2021-02-18 02:12:45 +0530

    added functions for add and subtract

---
+++ calculator.py
@@ -0,0 +1,9 @@
+"""
+Calculator lib having basic math operations
+"""
+def add(first_term, second_term):
+    return first_term + secnd_term
+
+def subtract(first_term, second_term):
+    return first_term - second_term
+
